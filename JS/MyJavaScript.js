ab3bf58a67a27250875bfbd9b0f4b82c31f82eec
Simas Dirzinauskas <sddirzius@gmail.com>
2022-12-12 15:35:40 +0200

    mano pirmasis komitas

---
+++ JS/MyJavaScript.js
@@ -0,0 +1,20 @@
+let x = 5;
+let y = 6;
+let z = x + y;
+console.log(z);
+
+let a = 1;
+console.log('Kintamojo a reikšmė: ', a);
+a = 2;
+console.log('Kintamojo a reikšmė: ', a);
+
+let b = "Simas Dirzinauskas";
+console.log ("Mano Vardas: ", b);
+
+let metai = 1994;
+let menesis = 9;
+let diena = 30;
+
+let sum = (metai / 10) + (menesis / 10) + (diena / 10);
+
+console.log('Sum of last digits of date of birth: ', sum);
